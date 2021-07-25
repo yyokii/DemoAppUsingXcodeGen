68bd41f83b0466f75349d7ea182c83277802066b
yyokii <yyokii.h@gmail.com>
2021-07-25 14:09:17 +0900

    Initial Commit

---
+++ DemoAppXcodeGen/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DemoAppXcodeGen
+//
+//  Created by Higashihara Yoki on 2021/07/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DemoAppXcodeGen/DemoAppXcodeGenApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoAppXcodeGenApp.swift
+//  DemoAppXcodeGen
+//
+//  Created by Higashihara Yoki on 2021/07/25.
+//
+
+import SwiftUI
+
+@main
+struct DemoAppXcodeGenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
